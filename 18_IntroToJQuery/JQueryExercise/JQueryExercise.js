0690525a747e55b9ec5d3c71a4bc6344fffa6630
Woojin Lee <dev.woojin.lee@gmail.com>
2020-02-26 16:17:14 +0900

    add jQuery exercise

---
+++ 18_IntroToJQuery/JQueryExercise/JQueryExercise.js
@@ -0,0 +1,11 @@
+// select all dis and give them a purple background
+$("div").css("background", "purple");
+
+// Select the divs with class "highlight" and make them 200px wide
+$("div.highlight").css("width", "200px");
+
+// Select the divs with id "third" and give it a orange border
+$("#third").css("border", "solid orange");
+
+// Select the first div only and change its font color to pink
+$("div:first-of-type")[0].css("color", "pink");
